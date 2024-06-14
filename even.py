3844c951cfe27ee9d76e84aa332d5540538facd9
mogamboxx <abhaysachan279@gmail.com>
2024-06-14 18:53:09 +0530

    files update

---
+++ even.py
@@ -0,0 +1,51 @@
+import tkinter as tk
+
+class TicTacToe:
+    def __init__(self):
+        self.window = tk.Tk()
+        self.window.title("Tic Tac Toe")
+        self.window.geometry("300x300")
+        self.player_turn = "X"
+
+        self.buttons = []
+        for i in range(3):
+            row = []
+            for j in range(3):
+                button = tk.Button(self.window, command=lambda row=i, column=j: self.click(row, column), height=3, width=6)
+                button.grid(row=i, column=j)
+                row.append(button)
+            self.buttons.append(row)
+
+    def click(self, row, column):
+        if self.buttons[row][column]['text'] == "":
+            self.buttons[row][column]['text'] = self.player_turn
+            if self.check_win():
+                self.window.title("Player " + self.player_turn + " wins!")
+                self.disable_buttons()
+            else:
+                self.player_turn = "O" if self.player_turn == "X" else "X"
+
+    def check_win(self):
+        for row in self.buttons:
+            if row[0]['text'] == row[1]['text'] == row[2]['text'] != "":
+                return True
+        for column in range(3):
+            if self.buttons[0][column]['text'] == self.buttons[1][column]['text'] == self.buttons[2][column]['text'] != "":
+                return True
+        if self.buttons[0][0]['text'] == self.buttons[1][1]['text'] == self.buttons[2][2]['text'] != "":
+            return True
+        if self.buttons[0][2]['text'] == self.buttons[1][1]['text'] == self.buttons[2][0]['text'] != "":
+            return True
+        return False
+
+    def disable_buttons(self):
+        for row in self.buttons:
+            for button in row:
+                button['state'] = 'disabled'
+
+    def run(self):
+        self.window.mainloop()
+
+if __name__ == "__main__":
+    game = TicTacToe()
+    game.run()
