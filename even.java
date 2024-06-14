68c4cb6f6228e2d0229a48ce215d2389ccaaea5d
mogamboxx <abhaysachan279@gmail.com>
2024-06-14 19:02:30 +0530

    java program updated

---
+++ even.java
@@ -0,0 +1,14 @@
+public class even
+{
+    public static void  main(String[] args) {
+        int num = 10;
+        if ((num&1)==0)
+        {
+         System.out.println("even");
+        }
+        else
+    {
+     System.out.println("odd");
+    }
+    }
+}
