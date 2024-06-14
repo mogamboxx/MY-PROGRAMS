7668ef98900343b3bcab4824cfbcabd837b17f3b
mogamboxx <abhaysachan279@gmail.com>
2024-06-14 19:05:01 +0530

    files updated

--- even.java
+++ even.java
@@ -2,7 +2,7 @@ public class even
 {
     public static void  main(String[] args) {
         int num = 10;
-        if ((num&1)==0)
+        if ((num&1)!=1)
         {
          System.out.println("even");
         }
